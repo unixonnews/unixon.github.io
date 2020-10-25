c52fb0823db1aae9d71afb2adef1283941003e58
unixonnews <68768923+unixonnews@users.noreply.github.com>
2020-10-25 16:01:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,6 +15,23 @@ $(function(){
 
 
 
+    // Mobile menu
+    $('.menu_open').click(function(){
+        $('.menu').css('display', 'block')
+        $('.menu').animate({left: '123px'}, 200)
+        $('.menu_background').fadeIn(600)
+        $('.close_mobile_menu').css('display', 'block')
+    })
+    $('.close_mobile_menu').click(function(){closeMenu()})
+
+    function closeMenu() {
+        $('.menu').animate({left: '500px'}, 200)
+        $('.menu_background').fadeOut(100)
+        $('.close_mobile_menu').css('display', 'none')
+    }
+
+    $('.menu_background').click(function(){closeMenu()})
+
 
     var userOutCheck = false;
     $('.arrow_down').click(function(){
@@ -50,7 +67,7 @@ $(function(){
     } else {
         $(".send_comment").css("background-color", "#3e37a4")
     }}, 50)}
-autosize($('.comment_write'));
+    autosize($('.comment_write'));
 
 $('.send_comment').click(function(){
     $('.comment_write').val("")
